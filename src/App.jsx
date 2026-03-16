6262eea0176f17d04a5a45b393910ff96fd5053d
Juan Camilo Barrios <147952337+JuanBarriosS@users.noreply.github.com>
2026-03-16 14:39:22 -0500

    nueva 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Login from './Login';
 import Admin from './Admin';
 import Empleado from './Empleado';
 import ProtectedRoute from "./ProtectedRoute";
+import LandingPage from './LandingPage'; 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/"       element={<LandingPage />} />  
+        <Route path="/login"  element={<Login />} />         
         <Route path="/admin" element={
           <ProtectedRoute rolesPermitidos={["ADMIN"]}>
             <Admin />
