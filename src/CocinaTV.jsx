b7ee0a179f48b19831e9070faf93dcf7f300b90e
Juan Camilo Barrios <147952337+JuanBarriosS@users.noreply.github.com>
2026-03-20 17:57:47 -0500

    save ultimo

--- src/CocinaTV.jsx
+++ src/CocinaTV.jsx
@@ -236,8 +236,8 @@ export default function CocinaTV() {
 
         <header className="tv-header">
           <div className="tv-logo">
-            <div className="tv-logo-icon">
-              <svg viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z"/></svg>
+            <div className="...logo-icon" style={{ fontSize: "20px" }}>
+                🫕
             </div>
             <div>
               <div className="tv-logo-name">Kitchen Manager</div>
