7f9d201858bc45221c249e39e1a6a4cc9bcd3efe
Juan Camilo Barrios <147952337+JuanBarriosS@users.noreply.github.com>
2026-04-11 16:30:01 -0500

    update imgs

--- src/LandinPage.jsx
+++ src/LandinPage.jsx
@@ -512,11 +512,11 @@ export default function LandingPage() {
             </div>
             <div className="lp-mosaic-img">
               <img
-                src="https://images.unsplash.com/photo-1608855238293-a8853e7f7c98?w=600&q=80&auto=format&fit=crop"
-                alt="Insumos y mise en place"
+                src="https://images.unsplash.com/photo-1568897798623-00bafffa9b1f?q=80&w=809&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="Facturacion"
                 loading="lazy"
               />
-              <div className="lp-mosaic-caption">Mise en place</div>
+              <div className="lp-mosaic-caption">Control en la facturacion</div>
             </div>
             <div className="lp-mosaic-img">
               <img
