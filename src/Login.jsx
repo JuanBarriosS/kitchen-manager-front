eb1740136f2c199531537a26aad084df377088ce
Juan Camilo Barrios <147952337+JuanBarriosS@users.noreply.github.com>
2026-04-23 00:26:08 -0500

    Change login URL from HTTPS to HTTP

--- src/Login.jsx
+++ src/Login.jsx
@@ -245,7 +245,7 @@ function Login() {
         setMensaje("");
 
         try {
-            const respuesta = await axios.post("https://100.96.106.39:7200/login", {
+            const respuesta = await axios.post("http://100.96.106.39:7200/login", {
                 username,
                 password
             });
