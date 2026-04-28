936e2c61efe4bc0e9d5c597ccec2a90c3bcc35d7
Juan Camilo Barrios <147952337+JuanBarriosS@users.noreply.github.com>
2026-04-28 18:44:03 -0500

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -245,7 +245,7 @@ function Login() {
         setMensaje("");
 
         try {
-            const respuesta = await axios.post("https://kitchen-manager-back-1-production.up.railway.app/login", {
+            const respuesta = await axios.post("https://kitchen-manager-back.onrender.com/login", {
                 username,
                 password
             });
