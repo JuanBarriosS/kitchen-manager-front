929634f0b71c99b1fc5585c2d6e10defee703e35
Juan Camilo Barrios <147952337+JuanBarriosS@users.noreply.github.com>
2026-04-29 13:49:08 -0500

    Rename MeseroView component to Mesero

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         <Route path="/cocina" element={<CocinaTV />} />
         <Route path="/menu/:token" element={<Clientes />} />
         <Route path="/seguimiento/:id" element={<SeguimientoPedido />} />
-        <Route path="/mesero" element={<MeseroView />} />
+        <Route path="/mesero" element={<Mesero />} />
         <Route path="/login"  element={<Login />} />         
         <Route path="/admin" element={
           <ProtectedRoute rolesPermitidos={["ADMIN"]}>
